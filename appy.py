f6e3ba7ce25d71e252c74bae8b37ea44abf29fa9
Özgür Düğmeci <ozgur.dugmeci@gmail.com>
2023-05-22 11:16:36 +0300

    Update appy.py

--- appy.py
+++ appy.py
@@ -151,6 +151,8 @@ if yan_sayfa_secenek == 'Analyses' :
   df_tutarlk.drop(['Kum'], inplace=True, axis=1)
   df_tutarlk.columns= ['Label','Product_Count','Ratio']
   df_tutarlk2=df_tutarlk.copy()
+  df_tutarlk2['Ratio']=df_tutarlk2['Ratio']*100
+  df_tutarlk2['Ratio']= df_tutarlk2['Ratio'].round(2)  
   df_tutarlk=df_tutarlk.sort_values(by='Ratio', ascending=False)
   total_product= str(total_product) + ' products analysed'
   #df_tutarlk['Product_Count']=df_tutarlk['Product_Count'].astype(str)
