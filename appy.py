50f4e1a3ca1499360f80f7fff5dc526f0a5c6980
Özgür Düğmeci <ozgur.dugmeci@gmail.com>
2023-05-22 11:06:28 +0300

    Update appy.py

--- appy.py
+++ appy.py
@@ -152,15 +152,16 @@ if yan_sayfa_secenek == 'Analyses' :
   df_tutarlk.columns= ['Label','Product_Count','Ratio']
   df_tutarlk=df_tutarlk.sort_values(by='Ratio', ascending=False)
   total_product= str(total_product) + ' products analysed'
-  df_tutarlk['Product_Count']=df_tutarlk['Product_Count'].astype(str)
+  #df_tutarlk['Product_Count']=df_tutarlk['Product_Count'].astype(str)
   #df_tutarlk['Ratio']=df_tutarlk['Ratio'].astype(str)
   #df.style.format("{:.2%}")
   #df.style.format({'B': "{:0<4.0f}", 'D': '{:+.2f}'})
-  #df_tutarlk= df_tutarlk.style.format({'Ratio': '{:.0%}'})
+  df_tutarlk= df_tutarlk.style.format({'Ratio': '{:.0%}'})
   st.info('A- Predictability Analysis') 
   total_product
-  #df_tutarlk2=df_tutarlk.astype(str).copy()
-  st.dataframe(df_tutarlk)
+  df_tutarlk2=df_tutarlk.copy()
+  df_tutarlk2=df_tutarlk2.astype(str)
+  st.dataframe(df_tutarlk2)
   'Predictability Analysis shows the quality of inventory management. The higher percentage of the "Predictable Sales" ratio indicates the good quality of the inventory management.'
   #download_data
   st.info('B- Inventory Planner')
